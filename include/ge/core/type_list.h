aa0a4e3886fd18946095e078610c94b9d3beb20a
Dmitry Shilnenkov <hogletgames@gmail.com>
2021-01-03 13:31:31 +0300

    [wip] ge: core: use Boost MPL for Type List

---
+++ include/ge/core/type_list.h
@@ -0,0 +1,59 @@
+/*
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2020, Dmitry Shilnenkov
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef GE_CORE_TYPE_LIST_H_
+#define GE_CORE_TYPE_LIST_H_
+
+#include <boost/mpl/contains.hpp>
+#include <boost/mpl/for_each.hpp>
+#include <boost/mpl/list.hpp>
+
+namespace GE {
+
+template<typename... Types>
+using TypeList = boost::mpl::list<Types...>;
+
+template<typename TL, typename Callback>
+constexpr void forEachType(Callback callback)
+{
+    boost::mpl::for_each<TL>(callback);
+}
+
+template<typename TL, typename T>
+constexpr bool isListContains()
+{
+    return boost::mpl::contains<TL, T>();
+}
+
+} // namespace GE
+
+#endif // GE_CORE_TYPE_LIST_H_
